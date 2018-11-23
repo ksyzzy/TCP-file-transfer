03eb0bed5ea321bb43d959ff706b32281fb7c9e1
_kushiji <xyzzy93@gmail.com>
2018-11-23 04:12:19 +0100

    Add files via upload

---
+++ src/Server/Program.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; // DLL support
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        [DllImport("SocketSupport.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int BeginServerOperation();
+        static void Main(string[] args)
+        {
+
+            BeginServerOperation();
+        }
+    }
+}
